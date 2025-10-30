2273862f9133483c5a115e8d0bd579f456e01055
ciemcornelissen <61663477+ciemcornelissen@users.noreply.github.com>
2025-10-30 12:41:41 +0000

    Deploying to gh-pages from @ ciemcornelissen/al-folio_test@7d7328cd20f5345bb1115568fb8a8ad6c8d5fc96 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,14 +501,35 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%63%69%65%6D.%63%6F%72%6E%65%6C%69%73%73%65%6E@%75%67%65%6E%74.%62%65", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/ciemcornelissen", "_blank");
+        },
+      },{
+        id: 'social-gitlab',
+        title: 'GitLab',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://gitlab.com/ciem.cornelissen", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0009-0003-0149-6722", "_blank");
+        },
+      },{
+        id: 'social-researchgate',
+        title: 'ResearchGate',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.researchgate.net/profile/Ciem-Cornelissen/", "_blank");
         },
       },{
         id: 'social-rss',
@@ -522,14 +543,14 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-work',
+        title: 'Work',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.ugent.be/ea/idlab/en", "_blank");
         },
       },{
       id: 'light-theme',
