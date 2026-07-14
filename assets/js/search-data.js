638c120904a6ec5f268828f349d527cba8fc9f5d
ciemcornelissen <61663477+ciemcornelissen@users.noreply.github.com>
2026-07-14 15:55:17 +0000

    Deploying to gh-pages from @ ciemcornelissen/ciemcornelissen.github.io@c2c1e777a0417e2a2ac1858181403aacf38da4a2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-curriculum-vitae",
           title: "Curriculum Vitae",
-          description: "Curriculum Vitae - AI Researcher specializing in Computer Vision and Hyperspectral Imaging",
+          description: "Curriculum Vitae - PhD researcher in Computer Science Engineering working on multi-modal self-supervised learning and autonomous perception",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
